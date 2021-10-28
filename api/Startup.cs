04e2d72f0523ea8307f581c1d3620fcfad1b948d
Nils Gustav Stråbø <nst@equinor.com>
2021-10-28 14:42:29 +0200

    disable https redirect

--- api/Startup.cs
+++ api/Startup.cs
@@ -44,8 +44,6 @@ namespace api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
