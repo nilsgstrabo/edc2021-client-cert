b6b6cbf359cd1fcbd9dfb04d69cc5f9dc07a5a8b
Nils Gustav Stråbø <nst@equinor.com>
2021-10-28 16:16:45 +0200

    swagger

--- api/Startup.cs
+++ api/Startup.cs
@@ -37,10 +37,10 @@ namespace api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
             }
 
